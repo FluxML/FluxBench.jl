15f73f6a60a81569cbca837a6765cd8103ebea85
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-03-28 13:38:34 +0530

    submit to codespeed

--- src/FluxBench.jl
+++ src/FluxBench.jl
@@ -8,4 +8,14 @@ const MODELS = (ResNet, DenseNet, GoogleNet, VGG19, SqueezeNet)
 include("benchmarkutils.jl")
 include("bench.jl")
 
+results = run(SUITE, verbose = true)
+println(results)
+
+flat_results = []
+flatten(results)
+
+HTTP.post("$(ENV["CODESPEED_SERVER"])/result/add/json/",
+          ["Content-Type" => "application/x-www-form-urlencoded"],
+          HTTP.URIs.escapeuri(Dict("json" => JSON.json(flat_results))))
+
 end # module
--- src/benchmarkutils.jl
+++ src/benchmarkutils.jl
@@ -11,3 +11,33 @@ macro async_benchmarkable(ex...)
 end
 
 SUITE = BenchmarkGroup()
+
+basedata = Dict(
+        "branch"        => ENV["CODESPEED_BRANCH"],
+        "commitid"      => ENV["CODESPEED_COMMIT"],
+        "project"       => ENV["CODESPEED_PROJECT"],
+        "environment"   => ENV["CODESPEED_ENVIRONMENT"],
+        "executable"    => ENV["CODESPEED_EXECUTABLE"]
+)
+
+# convert nested groups of benchmark to flat dictionaries of results
+function flatten(results, prefix = "")
+  for (key,value) in results
+    if value isa BenchmarkGroup
+      flatten(value, "$prefix$key/")
+    else
+      @assert value isa BenchmarkTools.Trial
+
+      # codespeed reports maxima, but those are often very noisy.
+      # get rid of measurements that unnecessarily skew the distribution.
+      rmskew!(value)
+
+      push!(flat_results,
+        Dict(basedata...,
+          "benchmark" => "$prefix$key",
+          "result_value" => median(value).time / 1e9,
+          "min" => minimum(value).time / 1e9,
+          "max" => maximum(value).time / 1e9))
+    end
+  end
+end
