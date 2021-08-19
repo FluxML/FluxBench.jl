5f25ab7ce925e7012877ddc47ba6b7f7e951ce41
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-08-20 03:02:06 +0530

    add method to commit

--- src/FluxBench.jl
+++ src/FluxBench.jl
@@ -1,7 +1,7 @@
 module FluxBench
 
-using Flux, Metalhead, ObjectDetector, DiffEqFlux
-using OrdinaryDiffEq, StochasticDiffEq, Distributions
+using Flux, Metalhead# , ObjectDetector, DiffEqFlux
+# using OrdinaryDiffEq, StochasticDiffEq, Distributions
 using BenchmarkTools, TimerOutputs
 using HTTP, JSON, FileIO
 using Flux.CUDA
@@ -14,18 +14,23 @@ const MODELS = (ResNet, DenseNet, GoogleNet, VGG19, SqueezeNet)
 SUITE = BenchmarkGroup()
 
 include("benchmarkutils.jl")
-include("packages/objectdetector.jl")
-include("packages/diffeqflux.jl")
+# include("packages/objectdetector.jl")
+# include("packages/diffeqflux.jl")
 include("bench.jl")
 
-results = run(SUITE, verbose = true)
-println(results)
 
-flat_results = []
-flatten(results)
+function submit(SUITE = SUITE)
+  bench()
+  warmup(SUITE, verbose = true)
+  results = run(SUITE, verbose = true)
+  println(results)
+  
+  flat_results = flatten(results)
 
-HTTP.post("$(ENV["CODESPEED_SERVER"])/result/add/json/",
-          ["Content-Type" => "application/x-www-form-urlencoded"],
-          HTTP.URIs.escapeuri(Dict("json" => JSON.json(flat_results))))
+  print(JSON.json(flat_results))
+  HTTP.post("$(ENV["CODESPEED_SERVER"])/result/add/json/",
+            ["Content-Type" => "application/x-www-form-urlencoded"],
+            HTTP.URIs.escapeuri(Dict("json" => JSON.json(flat_results))))
+end
 
 end # module
