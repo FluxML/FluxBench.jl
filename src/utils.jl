bef1e25edaace8df4ac0d3a8c7278d169611792c
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-08-26 19:06:16 +0530

    whitespace

--- src/utils.jl
+++ src/utils.jl
@@ -4,10 +4,10 @@ const REAL_RUN = get(ENV, "CODESPEED_BRANCH", nothing) == "master"
 
 # convenience macro to create a benchmark that requires synchronizing the GPU
 macro async_benchmarkable(ex...)
-    quote
-        # use non-blocking sync to reduce overhead
-        @benchmarkable CUDA.@sync blocking = false $(ex...)
-    end
+  quote
+    # use non-blocking sync to reduce overhead
+    @benchmarkable CUDA.@sync blocking = false $(ex...)
+  end
 end
 
 basedata = Dict(
