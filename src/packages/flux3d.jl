13653a999d2a5dc1e7e7ee6dc6826bf3e828453b
Nirmal Suthar <nirmalps@iitk.ac.in>
2021-08-31 02:09:17 +0530

    running locally

--- src/packages/flux3d.jl
+++ src/packages/flux3d.jl
@@ -1,5 +1,4 @@
-
-function trimesh_benchmark(mesh::TriMesh)
+function trimesh_benchmark_forward(mesh::TriMesh)
     get_edges_packed(mesh; refresh = true)
     get_faces_to_edges_packed(mesh; refresh = true)
     get_laplacian_packed(mesh; refresh = true)
@@ -7,6 +6,13 @@ function trimesh_benchmark(mesh::TriMesh)
     compute_verts_normals_packed(mesh)
     compute_faces_areas_packed(mesh)
     compute_faces_normals_packed(mesh)
+    l1 = laplacian_loss(mesh)
+    l2 = edge_loss(mesh)
+    l3 = chamfer_distance(mesh,mesh)
+    return l1+l2+l3
+end
+
+function trimesh_benchmark_back(mesh::TriMesh)
     l1 = laplacian_loss(mesh)
     l2 = edge_loss(mesh)
     l3 = chamfer_distance(mesh,mesh)
@@ -23,36 +29,36 @@ function flux3d_add_trimesh(f3d_grp)
         fw(m, mesh),
         setup = (
             mesh = TriMesh($mesh);
-            m = $trimesh_benchmark
+            m = $trimesh_benchmark_forward
         ),
-        teardown = (GC.fc(); CUDA.reclaim())
+        teardown = (GC.gc(); CUDA.reclaim())
     )
 
     f3d_grp["Flux3D_TriMesh_Backward_Pass"] = @benchmarkable(
         bw(m, mesh),
         setup = (
             mesh = TriMesh($mesh);
-            m = $trimesh_benchmark
+            m = $trimesh_benchmark_back
         ),
-        teardown = (GC.fc(); CUDA.reclaim())
+        teardown = (GC.gc(); CUDA.reclaim())
     )
    
     # f3d_grp["Flux3D_TriMesh_Forward_Pass_CUDA"] = @benchmarkable(
     #     fw(m, mesh),
     #     setup = (
     #         mesh = TriMesh($mesh) |> gpu;
-    #         m = $trimesh_benchmark
+    #         m = $trimesh_benchmark_forward
     #     ),
-    #     teardown = (GC.fc(); CUDA.reclaim())
+    #     teardown = (GC.gc(); CUDA.reclaim())
     # )
 
     # f3d_grp["Flux3D_TriMesh_Backward_Pass_CUDA"] = @benchmarkable(
     #     bw(m, mesh),
     #     setup = (
     #         mesh = TriMesh($mesh) |> gpu;
-    #         m = $trimesh_benchmark
+    #         m = $trimesh_benchmark_back
     #     ),
-    #     teardown = (GC.fc(); CUDA.reclaim())
+    #     teardown = (GC.gc(); CUDA.reclaim())
     # )
 
 end
