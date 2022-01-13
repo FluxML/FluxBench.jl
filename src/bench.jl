4bfc9cc9df8bb9bfe8a30609eef73b5359d93e47
Dhairya Gandhi <dhairya@juliacomputing.com>
2022-01-13 20:03:01 +0530

    up models name

--- src/bench.jl
+++ src/bench.jl
@@ -2,22 +2,22 @@
 
 group = addgroup!(SUITE, "Metalhead")
 
-function benchmark_cu(model, batchsize = 64)
+function benchmark_cu(model, batchsize = 64, config = nothing)
   resnet = model
   ip = rand(Float32, 224, 224, 3, batchsize)
 
-  group["Forward_Pass_$(model)_with_batchsize_$(batchsize)"] = b = @benchmarkable(
+  group["Forward_Pass_$(typeof(model))_$(config)_with_batchsize_$(batchsize)"] = b = @benchmarkable(
         fw(gresnet, gip),
         setup = (gresnet = $resnet |> gpu;
                gip = gpu($ip)),
         teardown = (GC.gc(); CUDA.reclaim()))
 end
 
-function benchmark_bw_cu(model, batchsize = 64)
+function benchmark_bw_cu(model, batchsize = 64, config = nothing)
   resnet = model
   ip = rand(Float32, 224, 224, 3, batchsize)
 
-  group["Backwards_Pass_$(model)_with_batchsize_$(batchsize)"] = b = @benchmarkable(
+  group["Backwards_Pass_$(typeof(model))_$(config)_with_batchsize_$(batchsize)"] = b = @benchmarkable(
         bw(gresnet, gip),
         setup = (gresnet = $resnet |> gpu;
    	       gip = gpu($ip)),
@@ -31,8 +31,9 @@ function bench()
     # but the CI machines would have variable VRAM
     # so be conservative
     # TODO: add larger batchsize for full benchmarking runs
-    benchmark_bw_cu(model(), n)
-    benchmark_cu(model(), n)
+    m, config = model
+    benchmark_bw_cu(m(), n, config)
+    benchmark_cu(m(), n, config)
   end
 
   # ObjectDetector
